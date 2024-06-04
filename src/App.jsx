336c90e630269a3868a48fac6c04bca05a51d619
Youssef <speedyoussef@gmail.com>
2024-06-04 23:19:07 +0100

    added nav

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,15 @@
-function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
-}
+import ButtonGradient from "./assets/svg/ButtonGradient";
+import Button from "./components/Button";
+
+const App = () => {
+  return (
+    <>
+      <div className="pl-3 pt-[4.75rem] lg:pt-{5.25rem] overflow-hidden">
+        <Header/>
+      </div>
+      <ButtonGradient/>
+    </>
+  );
+};
 
 export default App;
